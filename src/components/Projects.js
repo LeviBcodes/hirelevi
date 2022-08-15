445c4fb08261b0234b344a9d698e28c6ea7a4f71
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2022-08-15 01:08:11 -0600

    adding animation for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -50,25 +50,27 @@ const Projects = () => {
                 <p className='py-6'>Check out some of my recent projects</p>
             </div>
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                <div style={{backgroundImage: `url(${Moviedeets})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div group-hover:opacity-50'>
-                    {/* Hover effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-white tracking-wider px-3 mx-auto'>
-                            Moviedeets.com
-                        </span>
-                        <div className="pt-8 text-center">
-                            <button className="text-center rounded-lg px-4 py-1 m-2 bg-white text-gray-700 font-bold text-lg" onClick={()=>(modalOpen ? close() : open())}>View Demo</button>
+                <AnimatePresence>
+                    <motion.div style={{backgroundImage: `url(${Moviedeets})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div group-hover:opacity-50'>
+                        {/* Hover effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider px-3 mx-auto'>
+                                Moviedeets.com
+                            </span>
+                            <div className="pt-8 text-center">
+                                <button className="text-center rounded-lg px-4 py-1 m-2 bg-white text-gray-700 font-bold text-lg" onClick={()=>(modalOpen ? close() : open())}>View Demo</button>
+                            </div>
+                            <div className='pt-8 text-center'>
+                                <a href="http://www.moviedeets.com" target="_blank" rel="noreferrer">
+                                    <button className='text-center rounded-lg px-4 py-1 m-2 bg-white text-gray-700 font-bold text-lg'>Website</button>
+                                </a>
+                                <a href="https://www.github.com/LeviBCodes/moviedeets" target="_blank" rel="noreferrer">
+                                    <button className='text-center rounded-lg px-4 py-1 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                                </a>
+                            </div>
                         </div>
-                        <div className='pt-8 text-center'>
-                            <a href="http://www.moviedeets.com" target="_blank" rel="noreferrer">
-                                <button className='text-center rounded-lg px-4 py-1 m-2 bg-white text-gray-700 font-bold text-lg'>Website</button>
-                            </a>
-                            <a href="https://www.github.com/LeviBCodes/moviedeets" target="_blank" rel="noreferrer">
-                                <button className='text-center rounded-lg px-4 py-1 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                    </motion.div>
+                </AnimatePresence>
             </div>
             <AnimatePresence initial={false} exitBeforeEnter={true} onExitComplete={() => null}>
                 {modalOpen && <MovieDeetsModal modalOpen={modalOpen} handleClose={close} />}
